6b5367bd09531731dda78d5e154aa3de4a478b93
ayush-sharma52 <ayush052sharma@gmail.com>
2023-04-12 15:19:57 +0530

    router/tasks.js -> sortby wrapped in if stmnt

--- src/router/task.js
+++ src/router/task.js
@@ -32,9 +32,9 @@ router.get('/tasks',auth,async (req,res)=>{
     if(req.query.completed)
     match.completed= req.query.completed==='true';
 
-    const parts=req.query.sortBy.split(':');
     if(req.query.sortBy)
-    sort[parts[0]]= parts[1]==='desc'?-1:1;
+    {const parts=req.query.sortBy.split(':');
+    sort[parts[0]]= parts[1]==='desc'?-1:1;}
 
     
     try{
